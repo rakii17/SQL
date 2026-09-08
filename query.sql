b040b70592192e720be1eb76d7c6b7ac7f2098c4
Rakesh <rakesh.717.02@gmail.com>
2026-09-08 07:53:24 +0530

    ranking

--- query.sql
+++ query.sql
@@ -309,4 +309,12 @@ FROM
 	dim_product
 LIMIT 4;
 
-
+-- RANKING
+SELECT
+	unit_price,
+    ROW_NUMBER() OVER(ORDER BY unit_price) AS 'row_number',
+    RANK() OVER(ORDER BY unit_price) AS 'rank',
+	DENSE_RANK() OVER(ORDER BY unit_price) AS 'dense_rank'
+FROM
+	dim_product
+LIMIT 5 OFFSET 454;
